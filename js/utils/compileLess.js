17e756330f70d41c7398f4c247474bc8841cc2be
Lars Hiller Eidnes <larseidnes@gmail.com>
2015-09-02 13:26:16 +0200

    Fixed error handling for LESS compilation

--- js/utils/compileLess.js
+++ js/utils/compileLess.js
@@ -4,13 +4,16 @@ var fs = require('fs');
 
 module.exports = function compileLess(sourceFile, targetFile) {
 	function printError(err) { 
-		if (err) console.error("Error compiling LESS", err3);
+		if (err) console.error("Error compiling LESS: " + err);
 	}
 	fs.readFile(sourceFile, function(err1, data) {
     	printError(err1);
 	    less.render(data.toString(), function (err2, output) {
     		printError(err2);
-	        fs.writeFile(targetFile, output.css, printError);
+	        fs.writeFile(targetFile, output.css, function(err3) {
+	        	printError(err3);
+	        	if (!err1 && !err2 && !err3) console.debug("Successfully compiled LESS");
+	        });
 	    });
 	});
 }
