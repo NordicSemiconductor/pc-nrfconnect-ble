d78ca55b026770580aa9c2b755b0cbe4f29f20f9
Torleif Sandnes <torleif.sandnes@itema.no>
2015-09-02 09:28:34 +0200

    Refactored OSX fix for serialport

--- js/stores/bleTargetStore.js
+++ js/stores/bleTargetStore.js
@@ -5,6 +5,7 @@ import serialPort from 'serialport-electron';
 import child_process from 'child_process';
 
 import bleTargetActions from '../actions/bleTargetActions';
+import fs from 'fs';
 
 var bleTargetStore = reflux.createStore({
     listenables: [bleTargetActions],
@@ -19,39 +20,22 @@ var bleTargetStore = reflux.createStore({
     },
     onStartBleTargetDetect: function() {
         var self = this;
-        var portNum = 1;
         var portName = '';
         serialPort.list(function(err, ports) {
-            ports.forEach(function(port) {
+            self.discoveredBleTargets = ports.map(function(port){
                 portName = port.comName;
-                var modPortName = portName.replace('/dev/cu.', '/dev/tty.'); //workaround for wrong port location on darwin (OSX)
-                portExists(modPortName, portName, function(exists, modPortName, origPortName) {
-                    var portname;
-                    if (exists) {
-                        portname = modPortName;
-                     } else {
-                        portname = origPortName;
+                if (process.platform === 'darwin') {
+                    var modPortName = portName.replace('/dev/cu.', '/dev/tty.'); //workaround for wrong port location on darwin (OSX)
+                    if(fs.existsSync(modPortName)) {
+                        portName = modPortName;
                     }
-                    self.discoveredBleTargets.push({payload: '' + portNum, text: portname});
-                    portNum = portNum + 1;
-                    self.trigger(self.discoveredBleTargets);
-                });
+                }
+                return  {payload: 'maybenoutused', text: portName}; 
             });
+            self.discoveredBleTargets.unshift({payload: 'notused', text:'None'});
+            self.trigger(self.discoveredBleTargets);
         });
     }
 });
-
-var portExists = function(portName, origPortName, callback) {
-    child_process.exec('ls /dev/tty.*', function(err, stdout, stderr){
-        var listedPorts = stdout.split("\n").slice(9, -1);
-        var exists;
-        if (listedPorts.indexOf(portName) > -1) {
-            exists = true;
-        } else {
-            exists = false;
-        }
-        callback(exists, portName, origPortName)
-    });
-}
 
 module.exports = bleTargetStore;
