fe1285f62d91baa4a4de6597fea720b5154103d9
Simen Natvig <simen.natvig@nordicsemi.no>
2015-09-11 16:45:15 +0200

    Fixed math for line calculations.

--- js/components/Connector.jsx
+++ js/components/Connector.jsx
@@ -5,11 +5,11 @@ import React from 'react';
 var Connector = React.createClass({
     _calculateConnectorBox: function(sourceRect, targetRect) {
         var strokeWidth = 3;
-        var sourceRectMid = sourceRect.top - targetRect.top + sourceRect.height/2;
-        var targetRectMid = (targetRect.height/2);
+        this.sourceRectMid = sourceRect.top - targetRect.top + sourceRect.height/2;
+        this.targetRectMid = (targetRect.height/2);
 
-        var top = -(strokeWidth + 1)/2 + (sourceRectMid < targetRectMid ? sourceRectMid : targetRectMid);
-        var height = 2*((strokeWidth + 1)/2) + Math.abs(sourceRectMid - targetRectMid);
+        var top = -(strokeWidth + 1)/2 + (this.sourceRectMid < this.targetRectMid ? this.sourceRectMid : this.targetRectMid);
+        var height = 2*((strokeWidth + 1)/2) + Math.abs(this.sourceRectMid - this.targetRectMid);
         var width = targetRect.left - (sourceRect.left + sourceRect.width);
         return {
             top: top,
@@ -27,10 +27,13 @@ var Connector = React.createClass({
 
         var connectorBox = this._calculateConnectorBox(sourceRect, targetRect);
 
-        return (<svg style={{position: 'absolute', left: connectorBox.left,top: connectorBox.top, width:connectorBox.width, height:connectorBox.height}}>
-            <line x1="0" y1={connectorBox.height - 2} x2={connectorBox.width/2} y2={connectorBox.height - 2} stroke="black" strokeWidth="3" strokeLinecap="square"/>
-            <line x1={connectorBox.width/2} y1={connectorBox.height - 2} x2={connectorBox.width/2} y2="2" stroke="black" strokeWidth="3" strokeLinecap="square"/>
-            <line x1={connectorBox.width/2} y1="2" x2={connectorBox.width} y2="2" stroke="black" strokeWidth="3" strokeLinecap="square"/>
+        var sourceYCoordinate = this.sourceRectMid < this.targetRectMid ? 2 : connectorBox.height - 2;
+        var targetYCoordinate = this.sourceRectMid < this.targetRectMid ? connectorBox.height - 2 : 2;
+
+        return (<svg style={{position: 'absolute', left: connectorBox.left, top: connectorBox.top, width: connectorBox.width, height: connectorBox.height}}>
+            <line x1="0" y1={sourceYCoordinate} x2={connectorBox.width/2} y2={sourceYCoordinate} stroke="black" strokeWidth="3" strokeLinecap="square"/>
+            <line x1={connectorBox.width/2} y1={sourceYCoordinate} x2={connectorBox.width/2} y2={targetYCoordinate} stroke="black" strokeWidth="3" strokeLinecap="square"/>
+            <line x1={connectorBox.width/2} y1={targetYCoordinate}s x2={connectorBox.width} y2={targetYCoordinate} stroke="black" strokeWidth="3" strokeLinecap="square"/>
             </svg>);
     }
 });
