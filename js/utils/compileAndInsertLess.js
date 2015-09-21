15cc9a34ce1bce68f4861683d22d56a64b0690ce
Simen Natvig <simen.natvig@nordicsemi.no>
2015-09-21 12:41:49 +0200

    Changed compileAndInsertLess.js to look for styles.css in production location.

--- js/utils/compileAndInsertLess.js
+++ js/utils/compileAndInsertLess.js
@@ -10,22 +10,43 @@
  *
  */
 
-var compileLess = require("./js/utils/compileLess.js");
+var fs = require('fs');
+var compileLess = require('./js/utils/compileLess.js');
 
 //compile less and then insert css/styles.css
-function insertStyles() {
-    var link = document.createElement("link");
-    link.setAttribute("rel", "stylesheet");
-    link.setAttribute("type", "text/css");
-    link.setAttribute("href", "css/styles.css");
-    document.querySelector("head").appendChild(link);
+function insertStyles(cssPath) {
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('type', 'text/css');
+    link.setAttribute('href', cssPath);
+    document.querySelector('head').appendChild(link);
 }
-if (process.env.NODE_ENV === 'production') {
-    insertStyles();
-} else {
-    compileLess("css/styles.less", "css/styles.css", function(err) {
-        if (err) console.error("Error compiling LESS: " + err);
-        else console.debug("Successfully compiled LESS");
-        insertStyles();
-    });
+
+var stylesCssPath = 'resources/app/styles.css';
+
+if (process.platform === 'darwin') {
+    stylesCssPath = 'Resources/app/styles.css';
 }
+
+var stylesCssStats;
+
+fs.stat(stylesCssPath, function(err, stats) {
+    if (err) {
+        compileLess('css/styles.less', 'css/styles.css', function(err) {
+            if (err) {
+                console.error('Error compiling LESS: ' + err);
+            } else {
+                console.debug('Successfully compiled LESS');
+            }
+
+            insertStyles('css/styles.css');
+        });
+    } else {
+        if (stylesCssStats.isFile()) {
+            insertStyles(stylesCssPath);
+        } else {
+            console.error('Found css file but it is not a file')
+        }
+    }
+});
+
