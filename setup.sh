80f91127abc75a988846bc8701734c2ef3e5b3f1
Per Kristian Schanke <CoqRogue@users.noreply.github.com>
2015-09-21 14:10:34 +0200

    Cleaned up building as sqlite3 is fixed.

--- setup.sh
+++ setup.sh
@@ -5,25 +5,17 @@ npm install -g electron-packager
 npm install -g less
 
 export npm_config_runtime=electron
-export npm_config_runtime_version=0.30.3
+export npm_config_target=0.30.3
 export npm_config_arch=x64
 
-
-npm i --ignore-scripts sqlite3
-cd node_modules/sqlite3
-npm run prepublish
-node-gyp configure --module_name=node_sqlite3 --module_path=../lib/binding/node-v44-linux-x64
-node-gyp rebuild --target=0.30.3 --arch=x64 --target_platform=linux --dist-url=https://atom.io/download/atom-shell --module_name=node_sqlite3 --module_path=../lib/binding/node-v44-linux-x64
-cd ../..
-npm install
+npm install --production
 chown swdev_build: node_modules -R
-
 
 #flatten-packages
 
 lessc ./css/styles.less ./css/styles.css
 
-electron-packager ./ yggdrasil --platform=linux --arch=x64 --version=0.30.3 --out=../deploy2 --icon=Nordic_N_HI.ico --app-version=0.0.1 --version-string.CompanyName="Nordic Semiconductor" --version-string.LegalCopyright = "LegalCopyright" --version-string.FileDescription = "FileDescription" --version-string.OriginalFilename = "OriginalFilename" --version-string.FileVersion = "FileVersion" --version-string.ProductVersion = "ProductVersion" --version-string.ProductName = "ProductName" --version-string.InternalName = "InternalName"
+electron-packager ./ yggdrasil --platform=linux --arch=x64 --version=0.30.3 --out=../deploy --icon=Nordic_N_HI.ico --app-version=0.0.1 --version-string.CompanyName="Nordic Semiconductor" --version-string.LegalCopyright = "LegalCopyright" --version-string.FileDescription = "FileDescription" --version-string.OriginalFilename = "OriginalFilename" --version-string.FileVersion = "FileVersion" --version-string.ProductVersion = "ProductVersion" --version-string.ProductName = "ProductName" --version-string.InternalName = "InternalName"
 
 cp yggdrasil_installer.nsi ../deploy/
 
