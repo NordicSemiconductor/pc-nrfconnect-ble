5deb752bef51b09352203a6ab084f999dcadd4d8
Per Kristian Schanke <swdev@swdevs-Mac-mini.local>
2015-09-24 17:06:38 +0200

    Fixed copy paths for build script.

--- setup-darwin.sh
+++ setup-darwin.sh
@@ -11,6 +11,7 @@ export npm_config_arch=x64
 export YGGDRASIL_VERSION=0.7.0
 export YGGDRASIL_NAME=Yggdrasil
 export DEPLOY_DIR=../yggdrasil-deploy
+export YGGDRASIL_APP_ROOT_DIR=$DEPLOY_DIR/yggdrasil-darwin-$npm_config_arch
 
 rm -rf node_modules
 
@@ -20,8 +21,8 @@ flatten-packages
 lessc ./css/styles.less ./css/styles.css
 electron-packager . $YGGDRASIL_NAME --platform=darwin --arch=$npm_config_arch --version=$npm_config_target --overwrite --out=$DEPLOY_DIR --icon=nordic_logo.icns --app-version=$YGGDRASIL_VERSION --version-string.CompanyName="Nordic Semiconductor ASA" --version-string.LegalCopyright = "Nordic Semiconductor ASA" --version-string.FileDescription = "" --version-string.OriginalFilename = "" --version-string.FileVersion = "$YGGDRASIL_VERSION" --version-string.ProductVersion = "$YGGDRASIL_VERSION" --version-string.ProductName = "$YGGDRASIL_NAME" --version-string.InternalName = "$YGGDRASIL_NAME"
 
-cp README.md $DEPLOY_DIR/yggdrasil-win32-ia32/README.txt
-mkdir $DEPLOY_DIR/yggdrasil-win32-ia32/hex
-cp node_modules/pc-ble-driver-js/driver/hex/connectivity_115k2_with_s130_1.0.0.hex $DEPLOY_DIR/yggdrasil-win32-ia32/hex/connectivity_115k2_with_s130_1.0.0.hex
+cp README.md $YGGDRASIL_APP_ROOT_DIR/README.txt
+mkdir $YGGDRASIL_APP_ROOT_DIR/hex
+cp node_modules/pc-ble-driver-js/driver/hex/connectivity_115k2_with_s130_1.0.0.hex $YGGDRASIL_APP_ROOT_DIR/hex/connectivity_115k2_with_s130_1.0.0.hex
 
-tar czf $DEPLOY_DIR/$YGGDRASIL_NAME-$YGGDRASIL_VERSION-darwin-$npm_config_arch.tar.gz $DEPLOY_DIR/$YGGDRASIL_NAME-darwin-$npm_config_arch
+tar czf $DEPLOY_DIR/$YGGDRASIL_NAME-$YGGDRASIL_VERSION-darwin-$npm_config_arch.tar.gz $YGGDRASIL_APP_ROOT_DIR
