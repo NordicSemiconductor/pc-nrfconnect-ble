5837611a01e1e82e96465e572a72a44b72f472a5
kenr <ken.redergard@nordicsemi.no>
2015-09-24 17:26:37 +0200

    Linux: fixing copy paths for build script.

--- setup-linux-ubuntu.sh
+++ setup-linux-ubuntu.sh
@@ -9,8 +9,10 @@ export npm_config_target=0.30.3
 export npm_config_arch=x64
 
 export YGGDRASIL_VERSION=0.7.0
-export YGGDRASIL_NAME=Yggdrasil
+export YGGDRASIL_NAME=yggdrasil
 export DEPLOY_DIR=../yggdrasil-deploy
+
+export YGGDRASIL_APP_ROOT_DIR=$DEPLOY_DIR/yggdrasil-linux-$npm_config_arch
 
 rm -rf node_modules
 
@@ -20,8 +22,9 @@ flatten-packages
 lessc ./css/styles.less ./css/styles.css
 electron-packager . $YGGDRASIL_NAME --platform=linux --arch=$npm_config_arch --version=$npm_config_target --overwrite --out=$DEPLOY_DIR --app-version=$YGGDRASIL_VERSION --version-string.CompanyName = "Nordic Semiconductor ASA" --version-string.LegalCopyright = "Nordic Semiconductor ASA" --version-string.FileDescription = "" --version-string.OriginalFilename = "" --version-string.FileVersion = "$YGGDRASIL_VERSION" --version-string.ProductVersion = "$YGGDRASIL_VERSION" --version-string.ProductName = "$YGGDRASIL_NAME" --version-string.InternalName = "$YGGDRASIL_NAME"
 
-cp README.md $DEPLOY_DIR/yggdrasil-win32-ia32/README.txt
-mkdir $DEPLOY_DIR/yggdrasil-win32-ia32/hex
-cp node_modules/pc-ble-driver-js/driver/hex/connectivity_115k2_with_s130_1.0.0.hex $DEPLOY_DIR/yggdrasil-win32-ia32/hex/connectivity_115k2_with_s130_1.0.0.hex
+cp README.md $YGGDRASIL_APP_ROOT_DIR/README.txt
+mkdir $YGGDRASIL_APP_ROOT_DIR/hex
+cp node_modules/pc-ble-driver-js/driver/hex/connectivity_115k2_with_s130_1.0.0.hex $YGGDRASIL_APP_ROOT_DIR/hex/connectivity_115k2_with_s130_1.0.0.hex
 
-tar czf $DEPLOY_DIR/$YGGDRASIL_NAME-$YGGDRASIL_VERSION-linux-$npm_config_arch.tar.gz $DEPLOY_DIR/$YGGDRASIL_NAME-linux-$npm_config_arch
+tar czf $DEPLOY_DIR/$YGGDRASIL_NAME-$YGGDRASIL_VERSION-darwin-$npm_config_arch.tar.gz $YGGDRASIL_APP_ROOT_DIR
+
