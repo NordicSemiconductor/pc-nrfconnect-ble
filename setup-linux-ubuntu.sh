61a4066d2cd42298bf7f243eb150191a16e6440f
Simen Natvig <simen.natvig@nordicsemi.no>
2015-09-24 16:33:32 +0200

    Added readme and hex file copy for OSX and Linux.

--- setup-linux-ubuntu.sh
+++ setup-linux-ubuntu.sh
@@ -20,4 +20,8 @@ flatten-packages
 lessc ./css/styles.less ./css/styles.css
 electron-packager . $YGGDRASIL_NAME --platform=linux --arch=$npm_config_arch --version=$npm_config_target --overwrite --out=$DEPLOY_DIR --app-version=$YGGDRASIL_VERSION --version-string.CompanyName = "Nordic Semiconductor ASA" --version-string.LegalCopyright = "Nordic Semiconductor ASA" --version-string.FileDescription = "" --version-string.OriginalFilename = "" --version-string.FileVersion = "$YGGDRASIL_VERSION" --version-string.ProductVersion = "$YGGDRASIL_VERSION" --version-string.ProductName = "$YGGDRASIL_NAME" --version-string.InternalName = "$YGGDRASIL_NAME"
 
-tar czf $DEPLOY_DIR/$YGGDRASIL_NAME-$YGGDRASIL_VERSION-linux-$npm_config_arch.tar.gz $DEPLOY_DIR/$YGGDRASIL_NAME-linux-$npm_config_arch 
+cp README.md $DEPLOY_DIR/yggdrasil-win32-ia32/README.txt
+mkdir $DEPLOY_DIR/yggdrasil-win32-ia32/hex
+cp node_modules/pc-ble-driver-js/driver/hex/connectivity_115k2_with_s130_1.0.0.hex $DEPLOY_DIR/yggdrasil-win32-ia32/hex/connectivity_115k2_with_s130_1.0.0.hex
+
+tar czf $DEPLOY_DIR/$YGGDRASIL_NAME-$YGGDRASIL_VERSION-linux-$npm_config_arch.tar.gz $DEPLOY_DIR/$YGGDRASIL_NAME-linux-$npm_config_arch
