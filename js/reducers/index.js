a8e9fef85715a1b176dafe77722c2770f92d02d9
Simen Natvig <simen.natvig@nordicsemi.no>
2015-11-17 17:52:32 +0100

    Hooked navbar buttons and main view together and made App to a container.

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -5,11 +5,13 @@ import { combineReducers } from 'redux';
 import adapter from './adapterReducer';
 import discovery from './discoveryReducer';
 import log from './logReducer';
+import app from './appReducer';
 
 const rootReducer = combineReducers({
     adapter,
     discovery,
-    log
+    log,
+    app,
 });
 
 export default rootReducer;
