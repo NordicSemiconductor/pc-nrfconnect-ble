fe38fddba35f63fa8d39c45bea02b9fd6b69fa56
Per Kristian Schanke <per.kristian.schanke@nordicsemi.no>
2015-11-20 10:03:19 +0100

    Indentation issues.

--- js/actions/util/common.js
+++ js/actions/util/common.js
@@ -2,16 +2,16 @@
 
 // Utility functions
 export function getSelectedAdapter(state) {
-    if(state !== null && state !== undefined) {
+    if (state !== null && state !== undefined) {
         const root = state.adapter;
 
-        if(root !== null && root !== undefined) {
+        if (root !== null && root !== undefined) {
             const selectedAdapterIndex = root.selectedAdapter;
 
-            if(selectedAdapterIndex !== null && selectedAdapterIndex !== undefined) {
+            if (selectedAdapterIndex !== null && selectedAdapterIndex !== undefined) {
                 const adapter = state.adapter.adapters[selectedAdapterIndex];
 
-                if(adapter !== null && adapter !== undefined) {
+                if (adapter !== null && adapter !== undefined) {
                     return adapter;
                 }
             }
