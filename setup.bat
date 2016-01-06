63ba34404413d77c1a80cdaa1cf1498bc01e040a
Per Kristian Schanke <per.kristian.schanke@nordicsemi.no>
2016-01-06 15:18:10 +0100

    Fixed versioning. Also drops signing if not set up.

--- setup.bat
+++ setup.bat
@@ -9,7 +9,13 @@ call npm install -g less
 
 set VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120
 
-set YGGDRASIL_VERSION=0.9.0
+if ["%YGGDRASIL_VERSION%"] == [""] goto setVersion
+goto versionIsSet
+
+:setVersion
+set YGGDRASIL_VERSION master
+
+:versionIsSet
 set YGGDRASIL_DEPLOY_DIR=..\deploy
 set YGGDRASIL_ELECTRON_VERSION=0.35.4
 set YGGDRASIL_ELECTRON_ARCH=ia32
@@ -36,17 +42,21 @@ copy LICENSE %YGGDRASIL_DEPLOY_DIR%\yggdrasil-win32-ia32\LICENSE
 mkdir %YGGDRASIL_DEPLOY_DIR%\yggdrasil-win32-ia32\hex
 copy node_modules\pc-ble-driver-js\driver\hex\connectivity_115k2_with_s130_1.0.0.hex %YGGDRASIL_DEPLOY_DIR%\yggdrasil-win32-ia32\hex\connectivity_115k2_with_s130_1.0.0.hex
 
-cd ..\deploy
+cd %YGGDRASIL_DEPLOY_DIR%
 
 echo "Need to set SIGNTOOL_PATH and SIGNTOOL_PASSWORD environment variables for the next command to work"
+if ["%SIGNTOOL_PATH%"] == [""] goto buildInstaller
 %SIGNTOOL_PATH%\signtool.exe sign /v /ac %SIGNTOOL_PATH%\MSCV-VSClass3.cer /f %SIGNTOOL_PATH%\nordic_code_signing_certificate.pfx /p %SIGNTOOL_PASSWORD% /n "Nordic Semiconductor ASA" /t http://timestamp.verisign.com/scripts/timstamp.dll /d "Yggdrasil - %YGGDRASIL_VERSION%" ./yggdrasil-win32-ia32/yggdrasil.exe
 
+:buildInstaller
 "c:\Program Files (x86)\NSIS\makensis.exe" yggdrasil_installer.nsi
 rename yggdrasil_installer.exe yggdrasil_v%YGGDRASIL_VERSION%_installer.exe
 
 echo "Need to set SIGNTOOL_PATH and SIGNTOOL_PASSWORD environment variables for the next command to work"
+if ["%SIGNTOOL_PATH%"] == [""] goto cleanup
 %SIGNTOOL_PATH%\signtool.exe sign /v /ac %SIGNTOOL_PATH%\MSCV-VSClass3.cer /f %SIGNTOOL_PATH%\nordic_code_signing_certificate.pfx /p %SIGNTOOL_PASSWORD% /n "Nordic Semiconductor ASA" /t http://timestamp.verisign.com/scripts/timstamp.dll /d "Yggdrasil installer - %YGGDRASIL_VERSION%" yggdrasil_v%YGGDRASIL_VERSION%_installer.exe
 
+:cleanup
 popd
 
 rename js\settings.json settings.json.prod
