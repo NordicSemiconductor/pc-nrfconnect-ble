590bfc7f4870c1795a9dfcaa0f0c874307fe488c
Per Kristian Schanke <per.kristian.schanke@nordicsemi.no>
2016-02-17 16:34:21 +0100

    Updated to correct project names. Improved build process.

--- setup.bat
+++ setup.bat
@@ -1,5 +1,7 @@
 pushd %CD%
 
+rm -rf node_modules
+
 REM call npm install -g npm
 REM If you screw up your npm veresion (which may happen with 3.X series on Windows)
 REM follow these instructions https://www.npmjs.com/package/npm-windows-upgrade)
@@ -15,11 +17,11 @@ if ["%YGGDRASIL_VERSION%"] == [""] goto setVersion
 goto versionIsSet
 
 :setVersion
-set YGGDRASIL_VERSION master
+set YGGDRASIL_VERSION=0.0.0
 
 :versionIsSet
 set YGGDRASIL_DEPLOY_DIR=..\deploy
-set YGGDRASIL_ELECTRON_VERSION=0.36.1
+set YGGDRASIL_ELECTRON_VERSION=0.36.7
 set YGGDRASIL_ELECTRON_ARCH=ia32
 
 set npm_config_runtime=electron
@@ -34,29 +36,29 @@ call lessc ./css/styles.less ./css/styles.css
 rename js\settings.json settings.json.dev
 rename js\settings.json.prod settings.json
 
-call electron-packager ./ yggdrasil --platform=win32 --arch=%YGGDRASIL_ELECTRON_ARCH% --version=%YGGDRASIL_ELECTRON_VERSION% --overwrite --out=%YGGDRASIL_DEPLOY_DIR% --icon=nordic_logo.ico --app-version=%YGGDRASIL_VERSION% --version-string.CompanyName="Nordic Semiconductor" --version-string.LegalCopyright="Nordic Semiconductor" --version-string.FileDescription="FileDescription" --version-string.OriginalFilename="OriginalFilename" --version-string.FileVersion=%YGGDRASIL_VERSION% --version-string.ProductVersion=%YGGDRASIL_VERSION% --version-string.ProductName="Yggdrasil" --version-string.InternalName="Yggdrasil"
+call electron-packager ./ nrf-connect --platform=win32 --arch=%YGGDRASIL_ELECTRON_ARCH% --version=%YGGDRASIL_ELECTRON_VERSION% --overwrite --out=%YGGDRASIL_DEPLOY_DIR% --icon=nrfconnect.ico --app-version=%YGGDRASIL_VERSION% --version-string.CompanyName="Nordic Semiconductor" --version-string.LegalCopyright="Nordic Semiconductor" --version-string.FileDescription="nRF Connect" --version-string.OriginalFilename="nrf-connect.exe" --version-string.FileVersion=%YGGDRASIL_VERSION% --version-string.ProductVersion=%YGGDRASIL_VERSION% --version-string.ProductName="nRF Connect" --version-string.InternalName="nRF Connect"
 
 copy yggdrasil_installer.nsi %YGGDRASIL_DEPLOY_DIR%
-copy nordic_logo.ico %YGGDRASIL_DEPLOY_DIR%
-copy node_modules\pc-ble-driver-js\driver\lib\s130_nrf51_ble_driver.dll %YGGDRASIL_DEPLOY_DIR%\yggdrasil-win32-ia32
-copy README.md %YGGDRASIL_DEPLOY_DIR%\yggdrasil-win32-ia32\README.txt
-copy LICENSE %YGGDRASIL_DEPLOY_DIR%\yggdrasil-win32-ia32\LICENSE
-mkdir %YGGDRASIL_DEPLOY_DIR%\yggdrasil-win32-ia32\hex
-copy node_modules\pc-ble-driver-js\driver\hex\connectivity_115k2_with_s130_1.0.0.hex %YGGDRASIL_DEPLOY_DIR%\yggdrasil-win32-ia32\hex\connectivity_115k2_with_s130_1.0.0.hex
+copy nrfconnect.ico %YGGDRASIL_DEPLOY_DIR%
+copy node_modules\pc-ble-driver-js\driver\lib\s130_nrf51_ble_driver.dll %YGGDRASIL_DEPLOY_DIR%\nrf-connect-win32-ia32
+copy README.md %YGGDRASIL_DEPLOY_DIR%\nrf-connect-win32-ia32\README.txt
+copy LICENSE %YGGDRASIL_DEPLOY_DIR%\nrf-connect-win32-ia32\LICENSE
+mkdir %YGGDRASIL_DEPLOY_DIR%\nrf-connect-win32-ia32\hex
+copy node_modules\pc-ble-driver-js\driver\hex\connectivity_115k2_with_s130_1.0.0.hex %YGGDRASIL_DEPLOY_DIR%\nrf-connect-win32-ia32\hex\connectivity_115k2_with_s130_1.0.0.hex
 
 cd %YGGDRASIL_DEPLOY_DIR%
 
 echo "Need to set SIGNTOOL_PATH and SIGNTOOL_PASSWORD environment variables for the next command to work"
 if ["%SIGNTOOL_PATH%"] == [""] goto buildInstaller
-%SIGNTOOL_PATH%\signtool.exe sign /v /ac %SIGNTOOL_PATH%\MSCV-VSClass3.cer /f %SIGNTOOL_PATH%\nordic_code_signing_certificate.pfx /p %SIGNTOOL_PASSWORD% /n "Nordic Semiconductor ASA" /t http://timestamp.verisign.com/scripts/timstamp.dll /d "Yggdrasil - %YGGDRASIL_VERSION%" ./yggdrasil-win32-ia32/yggdrasil.exe
+%SIGNTOOL_PATH%\signtool.exe sign /v /ac %SIGNTOOL_PATH%\MSCV-VSClass3.cer /f %SIGNTOOL_PATH%\nordic_code_signing_certificate.pfx /p %SIGNTOOL_PASSWORD% /n "Nordic Semiconductor ASA" /t http://timestamp.verisign.com/scripts/timstamp.dll /d "nRF Connect - %YGGDRASIL_VERSION%" ./nrf-connect-win32-ia32/nrf-connect.exe
 
 :buildInstaller
 "c:\Program Files (x86)\NSIS\makensis.exe" yggdrasil_installer.nsi
-rename yggdrasil_installer.exe yggdrasil_v%YGGDRASIL_VERSION%_installer.exe
+rename nrf-connect_installer.exe nrf-connect_v%YGGDRASIL_VERSION%_installer.exe
 
 echo "Need to set SIGNTOOL_PATH and SIGNTOOL_PASSWORD environment variables for the next command to work"
 if ["%SIGNTOOL_PATH%"] == [""] goto cleanup
-%SIGNTOOL_PATH%\signtool.exe sign /v /ac %SIGNTOOL_PATH%\MSCV-VSClass3.cer /f %SIGNTOOL_PATH%\nordic_code_signing_certificate.pfx /p %SIGNTOOL_PASSWORD% /n "Nordic Semiconductor ASA" /t http://timestamp.verisign.com/scripts/timstamp.dll /d "Yggdrasil installer - %YGGDRASIL_VERSION%" yggdrasil_v%YGGDRASIL_VERSION%_installer.exe
+%SIGNTOOL_PATH%\signtool.exe sign /v /ac %SIGNTOOL_PATH%\MSCV-VSClass3.cer /f %SIGNTOOL_PATH%\nordic_code_signing_certificate.pfx /p %SIGNTOOL_PASSWORD% /n "Nordic Semiconductor ASA" /t http://timestamp.verisign.com/scripts/timstamp.dll /d "nRF Connect installer - %YGGDRASIL_VERSION%" nrf-connect_v%YGGDRASIL_VERSION%_installer.exe
 
 :cleanup
 popd
--- yggdrasil_installer.nsi
+++ yggdrasil_installer.nsi
@@ -8,13 +8,13 @@
 ;--------------------------------
 
 ; The name of the installer
-Name "Yggdrasil"
+Name "nRF Connect"
 
 ; The file to write
-OutFile "yggdrasil_installer.exe"
+OutFile "nrf-connect_installer.exe"
 
 ; The default installation directory
-InstallDir "$PROGRAMFILES\Nordic Semiconductor\Yggdrasil"
+InstallDir "$PROGRAMFILES\Nordic Semiconductor\nRF Connect"
 
 ; Request application privileges for Windows Vista
 RequestExecutionLevel admin
@@ -36,7 +36,7 @@ UninstPage instfiles
 ;--------------------------------
 
 ; The stuff to install
-Section "Yggdrasil (required)"
+Section "nRF Connect (required)"
 
   SectionIn RO
 
@@ -44,16 +44,16 @@ Section "Yggdrasil (required)"
   SetOutPath $INSTDIR
 
   ; Put file there
-  File /r yggdrasil-win32-ia32\*.*
+  File /r nrf-connect-win32-ia32\*.*
 
   ; Write the installation path into the registry
   ; WriteRegStr HKLM SOFTWARE\NSIS_Example2 "Install_Dir" "$INSTDIR"
 
   ; Write the uninstall keys for Windows
-  WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\Yggdrasil" "Yggdrasil" "Yggdrasil"
-  WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\Yggdrasil" "UninstallString" '"$INSTDIR\uninstall.exe"'
-  WriteRegDWORD HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\Yggdrasil" "NoModify" 1
-  WriteRegDWORD HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\Yggdrasil" "NoRepair" 1
+  WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\nRFConnect" "nRF Connect" "nRF Connect"
+  WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\nRFConnect" "UninstallString" '"$INSTDIR\uninstall.exe"'
+  WriteRegDWORD HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\nRFConnect" "NoModify" 1
+  WriteRegDWORD HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\nRFConnect" "NoRepair" 1
   WriteUninstaller "uninstall.exe"
 
 SectionEnd
@@ -62,9 +62,9 @@ SectionEnd
 Section "Start Menu Shortcuts"
 
   CreateDirectory "$SMPROGRAMS\Nordic Semiconductor"
-  CreateDirectory "$SMPROGRAMS\Nordic Semiconductor\Yggdrasil"
-  CreateShortcut "$SMPROGRAMS\Nordic Semiconductor\Yggdrasil\Uninstall.lnk" "$INSTDIR\uninstall.exe" "" "$INSTDIR\uninstall.exe" 0
-  CreateShortcut "$SMPROGRAMS\Nordic Semiconductor\Yggdrasil\Yggdrasil.lnk" "$INSTDIR\yggdrasil.exe"
+  CreateDirectory "$SMPROGRAMS\Nordic Semiconductor\nRF Connect"
+  CreateShortcut "$SMPROGRAMS\Nordic Semiconductor\nRF Connect\Uninstall.lnk" "$INSTDIR\uninstall.exe" "" "$INSTDIR\uninstall.exe" 0
+  CreateShortcut "$SMPROGRAMS\Nordic Semiconductor\nRF Connect\nRF Connect.lnk" "$INSTDIR\nrf-connect.exe"
 
 SectionEnd
 
@@ -75,15 +75,14 @@ SectionEnd
 Section "Uninstall"
 
   ; Remove registry keys
-  ; DeleteRegKey HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\Example2"
-  ; DeleteRegKey HKLM SOFTWARE\NSIS_Example2
+  DeleteRegKey HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\nRFConnect"
 
   ; Remove files and uninstaller
   RMDir /r $INSTDIR
   ; Delete $INSTDIR\uninstall.exe
 
   ; Remove shortcuts, if any
-  RMDir /r "$SMPROGRAMS\Nordic Semiconductor\Yggdrasil"
+  RMDir /r "$SMPROGRAMS\Nordic Semiconductor\nRF Connect"
 
   ; Remove directories used
   RMDir "$SMPROGRAMS\Nordic Semiconductor"
