66bd76fb8195ae9294091c48760c48eb39b1a927
Per Kristian Schanke <per.kristian.schanke@nordicsemi.no>
2016-02-19 17:39:43 +0100

    Added progress messages.

--- setup.bat
+++ setup.bat
@@ -5,39 +5,54 @@ rm -rf node_modules
 REM call npm install -g npm
 REM If you screw up your npm veresion (which may happen with 3.X series on Windows)
 REM follow these instructions https://www.npmjs.com/package/npm-windows-upgrade)
+echo "Installing global npm requirements"
 call npm install -g node-gyp
 call npm install -g electron-packager
 call npm install -g less
 
+echo "Setting up Visual Studio environment"
+
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
+echo "Setting up Visual Stuido Target Path"
 set VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120
 
+echo "Checking if version is set"
+
 if ["%YGGDRASIL_VERSION%"] == [""] goto setVersion
-goto versionIsSet
+echo "Version is set"
+goto versionSet
 
 :setVersion
+echo "Set version"
 set YGGDRASIL_VERSION=0.0.0
 
-:versionIsSet
+:versionSet
+echo "Setting environment variables"
 set YGGDRASIL_DEPLOY_DIR=..\deploy
 set YGGDRASIL_ELECTRON_VERSION=0.36.7
 set YGGDRASIL_ELECTRON_ARCH=ia32
 
+echo "Setting up electron information"
 set npm_config_runtime=electron
 set npm_config_target=%YGGDRASIL_ELECTRON_VERSION%
 set npm_config_arch=%YGGDRASIL_ELECTRON_ARCH%
 set npm_config_disturl=https://atom.io/download/atom-shell
 
+echo "Install production"
 call npm install --production
 
+echo "Run less"
 call lessc ./css/styles.less ./css/styles.css
 
+echo "Setting up release settings"
 rename js\settings.json settings.json.dev
 rename js\settings.json.prod settings.json
 
+echo "Copy driver"
 copy node_modules\pc-ble-driver-js\build\driver\Debug\pc-ble-driver.dll node_modules\pc-ble-driver-js\build\Debug\pc-ble-driver.dll
 
+echo "Packaging"
 call electron-packager ./ nrf-connect --platform=win32 --arch=%YGGDRASIL_ELECTRON_ARCH% --version=%YGGDRASIL_ELECTRON_VERSION% --overwrite --out=%YGGDRASIL_DEPLOY_DIR% --icon=nrfconnect.ico --app-version=%YGGDRASIL_VERSION% --version-string.CompanyName="Nordic Semiconductor" --version-string.LegalCopyright="Nordic Semiconductor" --version-string.FileDescription="nRF Connect" --version-string.OriginalFilename="nrf-connect.exe" --version-string.FileVersion=%YGGDRASIL_VERSION% --version-string.ProductVersion=%YGGDRASIL_VERSION% --version-string.ProductName="nRF Connect" --version-string.InternalName="nRF Connect"
 
 copy yggdrasil_installer.nsi %YGGDRASIL_DEPLOY_DIR%
@@ -50,14 +65,17 @@ copy node_modules\pc-ble-driver-js\driver\hex\connectivity_115k2_sdk-10.0.0_s130
 
 cd %YGGDRASIL_DEPLOY_DIR%
 
+echo "Sign executable"
 echo "Need to set SIGNTOOL_PATH and SIGNTOOL_PASSWORD environment variables for the next command to work"
 if ["%SIGNTOOL_PATH%"] == [""] goto buildInstaller
 %SIGNTOOL_PATH%\signtool.exe sign /v /ac %SIGNTOOL_PATH%\MSCV-VSClass3.cer /f %SIGNTOOL_PATH%\nordic_code_signing_certificate.pfx /p %SIGNTOOL_PASSWORD% /n "Nordic Semiconductor ASA" /t http://timestamp.verisign.com/scripts/timstamp.dll /d "nRF Connect - %YGGDRASIL_VERSION%" ./nrf-connect-win32-ia32/nrf-connect.exe
 
 :buildInstaller
+echo "Build installer"
 "c:\Program Files (x86)\NSIS\makensis.exe" yggdrasil_installer.nsi
 rename nrf-connect_installer.exe nrf-connect_v%YGGDRASIL_VERSION%_installer.exe
 
+echo "Sign installer"
 echo "Need to set SIGNTOOL_PATH and SIGNTOOL_PASSWORD environment variables for the next command to work"
 if ["%SIGNTOOL_PATH%"] == [""] goto cleanup
 %SIGNTOOL_PATH%\signtool.exe sign /v /ac %SIGNTOOL_PATH%\MSCV-VSClass3.cer /f %SIGNTOOL_PATH%\nordic_code_signing_certificate.pfx /p %SIGNTOOL_PASSWORD% /n "Nordic Semiconductor ASA" /t http://timestamp.verisign.com/scripts/timstamp.dll /d "nRF Connect installer - %YGGDRASIL_VERSION%" nrf-connect_v%YGGDRASIL_VERSION%_installer.exe
@@ -67,3 +85,5 @@ popd
 
 rename js\settings.json settings.json.prod
 rename js\settings.json.dev settings.json
+
+echo "Finished"
