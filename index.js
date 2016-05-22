06c2ab7aad8b48d419d71ae25d4a6ffea0527316
Bjørn Inge Hanssen <bjorn.inge.hanssen@nordicsemi.no>
2016-05-22 22:56:37 +0200

    Add accelerators to enable copy and paste on OSX

--- index.js
+++ index.js
@@ -117,6 +117,15 @@ app.once('ready', function() {
             ],
         },
         {
+            label: 'Edit',
+            submenu: [
+                { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+                { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+                { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+                { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
+            ],
+        },
+        {
             label: '&View',
             submenu: [
                 {
