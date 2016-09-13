58667f97f51169d85b5c93133c4e836061548093
Magne Rodem <magne@rodem.no>
2016-09-13 09:00:33 +0200

    Minify and optimize module order in production

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,5 +1,11 @@
+var webpack = require('webpack');
 var config = require('./webpack.config.development.js');
+
 config.entry.shift();
-config.plugins.shift();
+config.plugins = [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+];
 config.output.publicPath = './dist/';
+
 module.exports = config;
