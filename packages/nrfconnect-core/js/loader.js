0c9b61f83474c3ccc1cb62092af66826ec317e5c
Magne Rodem <magne@rodem.no>
2016-09-29 12:27:31 +0200

    Patch the Node module loader to avoid issues with peer dependencies
    React et.al is defined as peerDependencies in nrfconnect-core/package.json.
    These are installed at development time, but they are not bundled with the
    library, so those who depend on nrfconnect-core need to add their own React
    dependency in their package.json. This works fine when the library is built and
    published, but during development there will be a node_modules directory (with
    React) in nrfconnect-core, and this causes problems.
    
    When other projects import React components from nrfconnect-core, the Node
    module loader will first look for React in nrfconnect-core/node_modules. The
    other projects will provide their own React dependency, and that leads to two
    copies of React being loaded at the same time. In general, we do not want
    anything from nrfconnect-core/node_modules to be loaded by other projects. To
    avoid this we patch Node's module loader.
    
    For more info see:
    https://www.sharpoblunto.com/News/2016/01/25/testing-react-component-libraries-using-npm-link

---
+++ packages/nrfconnect-core/js/loader.js
@@ -0,0 +1,44 @@
+const m = require('module');
+const originalLoad = m._load;
+
+const packageConfig = require('../package.json');
+const peerDeps = packageConfig.peerDependencies;
+
+/**
+ We need to ensure that the users copy of peer dependencies are used, and not this
+ libraries own local copy (React component tests will fail as it doesn't like it
+ when multiple copies are loaded at once). To fix this we'll do some patching of
+ the Node module loader.
+
+ https://www.sharpoblunto.com/News/2016/01/25/testing-react-component-libraries-using-npm-link
+ */
+if (peerDeps) {
+    m._load = function (request, parent, isMain) {
+        if (peerDeps[request]) {
+            const parents = [];
+            while (parent) {
+                parents.push(parent);
+                parent = parent.parent;
+            }
+            // reverse the usual node module resolution. Instead
+            // of trying to load a local copy of the module and
+            // going up until we find one, we will try to resolve
+            // from the top down, this way peerDeps are preferentially
+            // loaded from the parent instead.
+            parent = parents.pop();
+            while (parent) {
+                try {
+                    return originalLoad(request, parent, isMain);
+                }
+                catch (ex) {
+                    parent = parents.pop();
+                }
+            }
+        } else {
+            return originalLoad(request, parent, isMain);
+        }
+    }
+}
+//Now export the library components
+module.exports = require('../dist/nrfconnect-core.min.js');
+m._load = originalLoad;
