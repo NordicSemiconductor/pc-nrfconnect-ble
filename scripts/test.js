6888a6b65e98fc25f2141d069fbde63d663361e6
Magne Rodem <magne@rodem.no>
2016-11-17 15:20:14 +0100

    Adjust test script so that it works on older Node versions

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,6 @@
 const spawn = require('./spawn');
 
 const sourceDirectory = 'js';
-const args = [sourceDirectory, ...process.argv.slice(2)];
+const args = [sourceDirectory].concat(process.argv.slice(2));
 
 spawn('jest', args, { BABEL_DISABLE_CACHE: 1 });
