8ab379512642403304a1d3d7e088f15fcb302475
Bence Frenyó <26139379+bencefr@users.noreply.github.com>
2020-04-06 07:57:52 +0200

    Update changelog and fix test warning

--- lib/components/__tests__/ConnectedDevice-test.jsx
+++ lib/components/__tests__/ConnectedDevice-test.jsx
@@ -60,6 +60,7 @@ function mountComponent(props) {
         connectedDevicesNumber={1}
         onPhyUpdate={() => {}}
         onMtuUpdate={() => {}}
+        onDataLengthUpdate={() => {}}
         {...props}
     />);
 }
