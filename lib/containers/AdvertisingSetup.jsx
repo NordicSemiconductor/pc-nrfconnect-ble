721b6969c2b3339b991990bb298e51e3bbc98cd3
Nicolai Berthelsen <nicolai.berthelsen@nordicsemi.no>
2020-11-02 12:18:49 +0100

    Remove the use of getters which didn't really contribute to anything

--- lib/containers/AdvertisingSetup.jsx
+++ lib/containers/AdvertisingSetup.jsx
@@ -65,9 +65,12 @@ class AdvertisingSetup extends React.PureComponent {
 
     findCurrentMaxId() {
         const { advSetup, scanResponse } = persistentStore;
-        const advId = advSetup.length > 0 ? advSetup.slice(-1)[0].id : 0;
+        const advSetupList = advSetup();
+        const scanResponseList = scanResponse();
+        const advId =
+            advSetupList.length > 0 ? advSetupList.slice(-1)[0].id : 0;
         const scanId =
-            scanResponse.length > 0 ? scanResponse.slice(-1)[0].id : 0;
+            scanResponseList.length > 0 ? scanResponseList.slice(-1)[0].id : 0;
         this.id = Math.max(advId, scanId);
     }
 
