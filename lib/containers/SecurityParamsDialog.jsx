396ee2e51cd674414383a1e38b01a4a2dde12b1a
Helene Lønvik <helene_t_l@hotmail.com>
2020-12-17 11:13:55 +0100

    Remove handleCancle from onHide in securityParamsDialo

--- lib/containers/SecurityParamsDialog.jsx
+++ lib/containers/SecurityParamsDialog.jsx
@@ -84,7 +84,7 @@ class SecurityParamsDialog extends React.PureComponent {
             <Modal
                 className="security-param-modal"
                 show={security.showingSecurityDialog}
-                onHide={() => this.handleCancel()}
+                onHide={() => {}}
             >
                 <Modal.Header>
                     <Modal.Title>Security parameters</Modal.Title>
