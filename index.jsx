13631fb94bc3ea1c385035b988d1410d4c1dd914
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2021-10-13 09:23:46 +0200

    feat: add documentation section to About pane
    Signed-off-by: Pascal Pickel <Pascal.Pickel@nordicsemi.no>

--- index.jsx
+++ index.jsx
@@ -11,6 +11,7 @@ import SidePanel from './lib/components/SidePanel';
 import BLEEventDialog from './lib/containers/BLEEventDialog';
 import DeviceDetails from './lib/containers/DeviceDetails';
 import DeviceSelector from './lib/containers/DeviceSelector';
+import DocumentationSections from './lib/containers/DocumentationSection';
 import ServerSetup from './lib/containers/ServerSetup';
 import reducers from './lib/reducers';
 import {
@@ -35,6 +36,7 @@ export default () => {
                 { name: 'Connection Map', Main: DeviceDetails },
                 { name: 'Server Setup', Main: ServerSetup },
             ]}
+            documentation={DocumentationSections}
         >
             <BLEEventDialog />
         </App>
