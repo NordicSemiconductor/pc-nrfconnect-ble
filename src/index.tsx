f51c95f36a7990eb1cedf518b4f4945c35083086
aadnekar <aadne.karstad@gmail.com>
2022-08-16 15:31:57 +0200

    Clean up the design for install screen
    Modified the spacing and outlay of the components in order to provide
    a better user experience in the proxy window.

--- src/index.tsx
+++ src/index.tsx
@@ -63,92 +63,125 @@ const Main = () => {
         setProgress(undefined);
     };
 
+    const isSamePath = (
+        executablePath: string,
+        directoryPath: string
+    ): boolean =>
+        executablePath.split('\\').slice(0, -1).join('\\') === directoryPath;
+
     return (
         <div className="d-flex h-100">
             <div className="floating-card">
-                <div className="title">Install</div>
-                <div className="content">
-                    <p className="py-3">
-                        {progress === undefined && (
-                            <>
-                                Bluetooth Low Energy is now a standalone app.
-                                {process.platform === 'win32' && (
-                                    <p>
-                                        The standalone application can be
-                                        uninstalled from the control panel.
-                                    </p>
-                                )}
-                            </>
-                        )}
-                        {progress !== undefined && (
-                            <>
-                                Downloading...
-                                <ProgressBar now={progress} />
-                            </>
-                        )}
-                    </p>
-
-                    {updateAvailable && (
-                        <div className="alert alert-info">
-                            Update available: Detected version: {version}, new
-                            version&nbsp;
-                            {bleVersion}
-                        </div>
-                    )}
-
-                    {exePath && (
+                <div className="title">
+                    {updateAvailable ? 'Update' : 'Install'}
+                </div>
+                <div className="content p-3 pt-4">
+                    {progress === undefined && (
                         <>
-                            <p className="text-muted">
-                                Your current installation is located at:{' '}
-                                {exePath}
+                            {updateAvailable && (
+                                <>
+                                    <p>An update is available</p>
+                                    <p className="mb-0">
+                                        Installed version: {version}.
+                                    </p>
+                                    <p>Available version: {bleVersion}.</p>
+                                </>
+                            )}
+
+                            <p>
+                                {!exePath && progress === undefined && (
+                                    <>
+                                        Bluetooth Low Energy is now a standalone
+                                        app.
+                                        {process.platform === 'win32' && (
+                                            <p className="mt-1">
+                                                The standalone application can
+                                                be uninstalled from the control
+                                                panel.
+                                            </p>
+                                        )}
+                                    </>
+                                )}
                             </p>
-                            <button
-                                type="button"
-                                className="btn btn-primary"
-                                onClick={runExecutable}
-                            >
-                                Launch current version
-                            </button>
+
+                            {exePath &&
+                                !isSamePath(exePath, programDirectory()) && (
+                                    <>
+                                        <p>
+                                            Your current installation is located
+                                            at:
+                                        </p>
+                                        <p className="path">{exePath}</p>
+                                    </>
+                                )}
+
+                            {errorMessage && (
+                                <div
+                                    className="alert alert-danger"
+                                    style={{ userSelect: 'text' }}
+                                >
+                                    {errorMessage}
+                                </div>
+                            )}
+
+                            {(exePath === undefined ||
+                                (updateAvailable && !errorMessage)) && (
+                                <>
+                                    <div className="d-flex flex-column mb-0">
+                                        <p>
+                                            The standalone application will be
+                                            installed at:
+                                        </p>
+                                        <p className="path">
+                                            {programDirectory()}
+                                        </p>
+                                        <p className="mb-4">
+                                            File size: {downloadSize}MB
+                                        </p>
+                                    </div>
+
+                                    <div className="d-flex justify-content-end">
+                                        {exePath && progress === undefined && (
+                                            <button
+                                                type="button"
+                                                className="btn btn-secondary font-size-12 mr-1"
+                                                onClick={runExecutable}
+                                            >
+                                                Open current version
+                                            </button>
+                                        )}
+                                        {progress === undefined && (
+                                            <button
+                                                type="button"
+                                                className="btn btn-primary font-size-12"
+                                                onClick={download}
+                                                style={{ width: '150px' }}
+                                            >
+                                                {updateAvailable
+                                                    ? 'Update Application'
+                                                    : 'Download and install'}
+                                            </button>
+                                        )}
+                                    </div>
+                                </>
+                            )}
                         </>
                     )}
-
-                    {errorMessage && (
-                        <div
-                            className="alert alert-danger"
-                            style={{ userSelect: 'text' }}
-                        >
-                            {errorMessage}
-                        </div>
-                    )}
-
-                    {(exePath === undefined ||
-                        (updateAvailable && !errorMessage)) && (
-                        <div className="d-flex">
-                            <div className="flex-grow-1 install-file-info">
-                                Target location: {programDirectory()}
-                                <br />
-                                File size: {downloadSize}MB
-                            </div>
-                            {progress === undefined && (
+                    {progress !== undefined && (
+                        <>
+                            <p>Downloading...</p>
+                            <ProgressBar now={progress} />
+                            <div className="d-flex justify-content-end mt-3">
                                 <button
                                     type="button"
-                                    className="btn btn-primary"
-                                    onClick={download}
-                                    style={{ width: '150px' }}
-                                >
-                                    Download and install
-                                </button>
-                            )}
-                            {progress !== undefined && (
-                                <button
-                                    type="button"
-                                    className="btn btn-outline-dark"
+                                    className="btn btn-outline-dark font-size-12"
                                     onClick={cancel}
+                                    style={{ width: '150px' }}
                                 >
                                     Cancel
                                 </button>
-                            )}
-                        </div>
+                            </div>
+                        </>
                     )}
                 </div>
             </div>
